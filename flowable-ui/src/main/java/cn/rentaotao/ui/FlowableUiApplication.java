13529f66288bab82053ea58fd8d610130fb44c55
rtt <1191623980@qq.com>
2025-04-08 23:21:56 +0800

    0408

---
+++ flowable-ui/src/main/java/cn/rentaotao/ui/FlowableUiApplication.java
@@ -0,0 +1,12 @@
+package cn.rentaotao.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableUiApplication.class, args);
+    }
+}
